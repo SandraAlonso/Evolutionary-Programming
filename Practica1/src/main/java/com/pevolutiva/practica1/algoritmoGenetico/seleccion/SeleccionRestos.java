3dd17289056c27908e4745581731b011625c1aae
sandraalonso <sandraalopa@gmail.com>
2021-03-12 13:36:41 +0100

    Acabada selección por restos :)

--- Practica1/src/main/java/com/pevolutiva/practica1/algoritmoGenetico/seleccion/SeleccionRestos.java
+++ Practica1/src/main/java/com/pevolutiva/practica1/algoritmoGenetico/seleccion/SeleccionRestos.java
@@ -18,27 +18,38 @@ public class SeleccionRestos extends Seleccion {
 
 	@Override
 	public List<Individuo> run(List<Individuo> poblacion) {
-            
-            Double suma = 0.0;
-            for (Individuo ind : poblacion) {
-                    suma += ind.getValor();
-            }
-            Integer numIndSelec = poblacion.size();
-            Random rand = new Random();
-            List<Individuo> NuevaPoblacion = new ArrayList<Individuo>();
-            
-            
-            for(int i = 0; i < poblacion.size(); ++i){
-                if((poblacion.get(i).getValor() / suma) * poblacion.size() > 1){
-                    int nuevosInd = (int) Math.ceil(poblacion.get(i).getValor() / suma);
-                    for(int j = 0; j < nuevosInd; j++){
-                        NuevaPoblacion.add(poblacion.get(i));
-                    }
-                }
-            }
-            
-            
-            return NuevaPoblacion;
+
+		Double suma = 0.0;
+
+		for (Individuo ind : poblacion) {
+			suma += ind.getValor();
+		}
+
+		Integer numIndSelec = poblacion.size();
+		Random rand = new Random();
+
+		List<Individuo> NuevaPoblacion = new ArrayList<Individuo>();
+
+		for (int i = 0; i < poblacion.size(); ++i) {
+			if ((poblacion.get(i).getValor() / suma) * poblacion.size() > 1) {
+				// Redondeamos hacia arriba
+				int nuevosInd = (int) Math.ceil(poblacion.get(i).getValor() / suma);
+				for (int j = 0; j < nuevosInd; j++) {
+					NuevaPoblacion.add(poblacion.get(i));
+				}
+			}
+		}
+		if (NuevaPoblacion.size() < poblacion.size()) {// Tenemos que rellenar por otro metodo
+			SeleccionTorneo s = new SeleccionTorneo(); // Escogemos torneo porque así nos aseguramos que quedan los
+														// mejores y además nos proporciona copias repetidas
+			List<Individuo> poblacionExtra = s.run(poblacion);
+			int diferencia = poblacion.size() - NuevaPoblacion.size();
+			for (int i = 0; i < diferencia; i++) {
+				NuevaPoblacion.add(poblacionExtra.get(i));
+			}
+		}
+
+		return NuevaPoblacion;
 	}
 
 }
