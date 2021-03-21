3019d06a88615195c776fcc924944da2d74f3902
sandraalonso <sandraalopa@gmail.com>
2021-03-21 20:01:03 +0100

    Nueva gui

---
+++ Practica1/src/main/java/com/pevolutiva/practica1/view/Grafica.java
@@ -0,0 +1,47 @@
+package com.pevolutiva.practica1.view;
+
+import javax.swing.JFrame;
+
+import javax.swing.*;
+import org.math.plot.*;
+public class Grafica {
+
+	
+	public void crearGrafica() {
+		// define your data
+				double[] x = { 1, 2, 3, 4, 5, 6 };
+				double[] y = { 45, 89, 6, 32, 63, 12 };
+
+				// create your PlotPanel (you can use it as a JPanel)
+				Plot2DPanel plot = new Plot2DPanel();
+
+				// define the legend position
+				plot.addLegend("SOUTH");
+
+				// add a line plot to the PlotPanel
+				plot.addLinePlot("my plot", x, y);
+
+
+		
+	}
+	/*public static void main(String[] args) {
+		// define your data
+		double[] x = { 1, 2, 3, 4, 5, 6 };
+		double[] y = { 45, 89, 6, 32, 63, 12 };
+
+		// create your PlotPanel (you can use it as a JPanel)
+		Plot2DPanel plot = new Plot2DPanel();
+
+		// define the legend position
+		plot.addLegend("SOUTH");
+
+		// add a line plot to the PlotPanel
+		plot.addLinePlot("my plot", x, y);
+
+		// put the PlotPanel in a JFrame like a JPanel
+		JFrame frame = new JFrame("a plot panel");
+		frame.setSize(600, 600);
+		frame.setContentPane(plot);
+		frame.setVisible(true);
+		}*/
+}
