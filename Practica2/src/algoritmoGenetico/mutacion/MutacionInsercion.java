570eed8eceaec7aac6cc79c42fdf814bde59a2a4
sandraalonso <sandraalopa@gmail.com>
2021-04-20 12:25:06 +0200

    Mutacion por inserccion, intercambio e inversion

---
+++ Practica2/src/algoritmoGenetico/mutacion/MutacionInsercion.java
@@ -0,0 +1,36 @@
+package algoritmoGenetico.mutacion;
+
+import java.util.List;
+import java.util.Random;
+
+import algoritmoGenetico.individuos.Individuo;
+
+public class MutacionInsercion extends Mutacion {
+
+	@Override
+	public List<Individuo> run(List<Individuo> poblacion, Double porcentMut) {
+		Random rand = new Random();
+		int posicionesACambiar = rand.nextInt(5)+1;
+		for (int i = 0; i < poblacion.size(); i++) {
+			Double[] c = (Double[]) poblacion.get(i).getCromosoma();
+			Double alt = rand.nextDouble();
+			if (porcentMut > alt) { // mutamos
+				for(int j =0;j<posicionesACambiar;j++) {
+					int posActual = rand.nextInt(26);
+					int posFinal = rand.nextInt(posActual);
+					for(int k =posFinal+1;k<posActual;k++) {
+						c[k]=c[k-1];
+					}
+					c[posFinal]=c[posActual];
+					c[posActual]=c[posActual-1];
+					
+				}
+				poblacion.get(i).setCromosoma(c);
+
+			}
+
+		}
+		return poblacion;
+	}
+
+}
